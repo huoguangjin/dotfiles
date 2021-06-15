26d2da3f9c8edce6235ac302ab1fc00319645b04
huoguangjin <352886006@qq.com>
2021-06-15 09:45:09 +0800

    fix(flag): add python flag

--- flag.sh
+++ flag.sh
@@ -3,5 +3,10 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 
 export RIPGREP_CONFIG_PATH=$DOTFILES/.ripgreprc
 
+export PYTHONUNBUFFERED=1
+export PYTHONBREAKPOINT="ipdb.set_trace()"
+
+export PIPENV_VENV_IN_PROJECT=1
 # export PYENV_ROOT="$DOTFILES/.pyenv"
+# export POETRY_HOME=$HOME/.poetry
 
