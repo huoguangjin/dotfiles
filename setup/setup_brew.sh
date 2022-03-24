a0d94e13ec2100cea76b066ddfe037159c3e3ea0
huoguangjin <352886006@qq.com>
2022-03-24 15:08:49 +0800

    feat(setup): add path setup script

--- setup/setup_brew.sh
+++ setup/setup_brew.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mkdir -p /usr/local/{bin,lib}
-mkdir -p /opt/local/{bin,lib}
-
-# brew --prefix # /opt/homebrew
-export PATH="/opt/homebrew/bin:$PATH"
-export PATH="/usr/local/bin:$PATH"
-export PATH="/opt/local/bin:$PATH"
-
 # https://brew.sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zshrc
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+echo ">> brew info"
+which brew
+
+# /opt/homebrew
+brew --repo
+
+# /opt/homebrew
+brew --prefix
 
 # cd $(brew --repo)/Library/Taps/homebrew # cd /opt/homebrew/Library/Taps/homebrew
 # git clone --depth=1 https://github.com/Homebrew/homebrew-core.git
---
+++ setup/setup_path.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export DOTFILES="$(dirname $0)/.."
+
+mkdir -p /usr/local/{bin,lib}
+mkdir -p /opt/local/{bin,lib}
+
+export PATH="/usr/local/bin:$PATH"
+export PATH="/opt/local/bin:$PATH"
+# export PATH="/opt/homebrew/bin:$PATH"
