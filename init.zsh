1e396f8f83ffefa100b794a28a5b6a8376815702
huoguangjin <352886006@qq.com>
2022-03-24 22:24:26 +0800

    feat(zsh): add init.zsh

---
+++ init.zsh
@@ -0,0 +1,22 @@
+
+export DOTFILES="$(dirname "$0")"
+# export XDG_CONFIG_HOME="$DOTFILES"
+
+source $DOTFILES/flag.sh
+
+source $DOTFILES/alias.sh
+
+source $DOTFILES/env.sh
+
+source $DOTFILES/fun.sh
+
+# $(brew --prefix fzf)/install
+source $DOTFILES/fzf.zsh
+source $DOTFILES/fzf_fun.sh
+
+[ -f $DOTFILES/tx.sh ] && source $DOTFILES/tx.sh
+
+# key binding
+bindkey '^k' push-line-or-edit
+
+export PATH="$PATH:$HOME/bin"
