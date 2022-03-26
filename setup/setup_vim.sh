91c4a1dd65736741516dd1d225e9f103f4ab227d
huoguangjin <352886006@qq.com>
2022-03-26 20:13:12 +0800

    feat(setup): add vim setup script

---
+++ setup/setup_vim.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+mkdir -p $HOME/.cache/vim/{backup,swap,undo}
+
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +'PlugInstall --sync' +qa
