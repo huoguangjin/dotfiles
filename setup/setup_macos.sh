106460eff0e103ab73ce1370521b3df81feaf174
huoguangjin <352886006@qq.com>
2022-04-03 13:48:05 +0800

    feat(setup): set macos preferences from cmd

--- setup/setup_macos.sh
+++ setup/setup_macos.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# enable key repeating
+defaults write -g ApplePressAndHoldEnabled -bool false
 
+defaults write com.apple.finder AppleShowAllFiles true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+killall Finder
+
+
+# hide Dock immediately
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -int 0
+
+# System Preferences > Dock & Menu Bar > Dock & Menu Bar > Minimize windows using > Scale effect
+defaults write com.apple.dock mineffect scale
+
+killall Dock
