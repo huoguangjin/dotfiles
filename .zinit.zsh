c6f037c4f11abd8ee2f17f24bd20630c79c6cd1e
huoguangjin <352886006@qq.com>
2022-04-04 08:23:53 +0800

    feat(brew): install brew from zinit

--- .zinit.zsh
+++ .zinit.zsh
@@ -9,6 +9,21 @@ setopt pushdminus
 setopt interactivecomments
 
 
+zinit ice id-as="brew" \
+  atclone='/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [[ -x /opt/homebrew/bin/brew ]]; then
+  /opt/homebrew/bin/brew shellenv | tee brew.zsh
+elif [[ -x /opt/local/bin/brew ]]; then
+  /opt/local/bin/brew shellenv | tee brew.zsh
+elif [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+  /home/linuxbrew/.linuxbrew/bin/brew shellenv | tee brew.zsh
+else
+  echo > brew.zsh
+fi' \
+  atpull='%atclone' run-atpull \
+  nocompile'!'
+zinit light zdharma-continuum/null
+
 zinit snippet OMZ::lib/termsupport.zsh
 
 zinit wait lucid light-mode for \
--- setup/setup_brew.sh
+++ setup/setup_brew.sh
@@ -3,21 +3,24 @@ set -euo pipefail
 
 # https://brew.sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zshrc
-eval "$(/opt/homebrew/bin/brew shellenv)"
 
-echo ">> brew info"
-which brew
+if [[ -x /opt/homebrew/bin/brew ]]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [[ -x /opt/local/bin/brew ]]; then
+  eval "$(/opt/local/bin/brew shellenv)"
+elif [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+else
+  function brew() {
+    echo ">> brew not found"
+  }
+fi
 
-# /opt/homebrew
-brew --repo
-
-# /opt/homebrew
-brew --prefix
+echo ">> brew shellenv"
+brew shellenv
 
 # cd $(brew --repo)/Library/Taps/homebrew # cd /opt/homebrew/Library/Taps/homebrew
 # git clone --depth=1 https://github.com/Homebrew/homebrew-core.git
 # git clone --depth=1 https://github.com/Homebrew/homebrew-cask.git
 # git -C homebrew-core fetch --unshallow
 # git -C homebrew-cask fetch --unshallow
-
