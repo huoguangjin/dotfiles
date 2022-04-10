7eed6cfdc84743f2746a669c002dee66e446160d
huoguangjin <352886006@qq.com>
2022-04-10 10:59:22 +0800

    feat(setup_vim): move plug.vim to $HOME/.local/share/vim/autoload

--- setup/setup_vim.sh
+++ setup/setup_vim.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 mkdir -p $HOME/.cache/vim/{backup,swap,undo}
 
-curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+curl -fLo $HOME/.local/share/vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +'PlugInstall --sync' +qa
