4a3faddc2707d468704a4dc994c867e265cc36c4
huoguangjin <352886006@qq.com>
2022-04-16 14:46:43 +0800

    feat(setup_keylayout): open Keyboard system preferences after setup

--- setup/setup_keylayout.sh
+++ setup/setup_keylayout.sh
@@ -4,3 +4,8 @@ set -euo pipefail
 export DOTFILES=$(cd "$(dirname "$0")/.."; pwd -P)
 
 cp "$DOTFILES/keylayout/raindrop.keylayout" "$HOME/Library/Keyboard Layouts/"
+
+echo "Open System Preferences and change input source:
+Preferences > Keyboard > Input Sources > Add > Others > raindrop
+"
+open -b com.apple.systempreferences /System/Library/PreferencePanes/Keyboard.prefPane
