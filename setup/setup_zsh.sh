6e04f4f78da4779f051e2db00c2e5419eed6682a
huoguangjin <352886006@qq.com>
2022-12-26 23:46:05 +0800

    feat(setup_zsh): backup old .zshrc

--- setup/setup_zsh.sh
+++ setup/setup_zsh.sh
@@ -7,7 +7,9 @@ ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 mkdir -p "$(dirname "$ZINIT_HOME")"
 git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 
-cp "$HOME"/.zshrc "$DOTFILES"/backup
-cp "$DOTFILES"/.zshrc "$HOME"
+rsync -arcv \
+  -b --backup-dir "$DOTFILES"/backup \
+  "$DOTFILES"/.zshrc \
+  "$HOME"/.zshrc
 
 zsh
