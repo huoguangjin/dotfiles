c2391acfdb4464b5d92459044b848df6a171c77f
huoguangjin <352886006@qq.com>
2022-12-31 17:45:12 +0800

    feat(ipython): add preload modules

--- .ipython/profile_default/startup/preload.py
+++ .ipython/profile_default/startup/preload.py
@@ -1,12 +1,16 @@
-#!/usr/bin/env python3
-
+# coding: utf-8
+import asyncio
 import json
 import math
 import os
+import re
 import sys
+from datetime import date, datetime, timezone
 from pathlib import Path
 from urllib.parse import urljoin, urlparse
 
-# import numpy as np
-# import pandas as pd
-
+try:
+    import numpy as np
+    import pandas as pd
+except ImportError:
+    pass
