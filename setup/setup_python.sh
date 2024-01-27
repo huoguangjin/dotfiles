ba771332f6fbcfda57534fe679ab066e249285d9
huoguangjin <352886006@qq.com>
2024-01-27 17:07:07 +0800

    feat(setup): update setup_python.sh

--- setup/setup_python.sh
+++ setup/setup_python.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-pyenv install 3.10.3
+pyenv install 3.12.4
 pyenv install 2.7.18
 pyenv rehash
-pyenv global 3.10.3 2.7.18
+pyenv global 3.12.4 2.7.18
 
 # https://pypa.github.io/pipx/docs/
 export PIPX_DEFAULT_PYTHON=$(pyenv which python)
@@ -14,3 +14,5 @@ poetry config virtualenvs.in-project true
 
 pipx install ipython
 pipx install black
+pipx install ranger-fm
+pipx install conan
