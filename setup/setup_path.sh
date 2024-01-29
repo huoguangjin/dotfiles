da799fb13f37adc47795bdaae63e9678b7aa1df5
huoguangjin <352886006@qq.com>
2024-01-29 19:51:51 +0800

    feat(setup): clean up setup_path.sh

--- setup/setup_path.sh
+++ setup/setup_path.sh
@@ -2,10 +2,3 @@
 set -euo pipefail
 
 export DOTFILES=$(realpath "$0"/../..)
-
-mkdir -p /usr/local/{bin,lib}
-mkdir -p /opt/local/{bin,lib}
-
-export PATH="/usr/local/bin:$PATH"
-export PATH="/opt/local/bin:$PATH"
-# export PATH="/opt/homebrew/bin:$PATH"
