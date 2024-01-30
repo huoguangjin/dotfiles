ffd692f4bcc85548c4e11bf7ad2227d1cff80cb0
huoguangjin <352886006@qq.com>
2024-01-30 19:16:16 +0800

    feat(setup): update macos app

--- setup/setup_macos_app.sh
+++ setup/setup_macos_app.sh
@@ -1,37 +1,65 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+cat <<EOF
+==================== AppStore app ====================
+iShot
+iOCR
+==================== other app ====================
+https://developer.android.google.cn/studio
+https://karabiner-elements.pqrs.org
+https://antares-sql.app/
+EOF
+
 export HOMEBREW_NO_AUTO_UPDATE=1
 
 brew update
 
-cask=(
-  google-chrome
-  iterm2
-  karabiner-elements
-  keyboard-maestro
-  moom
-  v2rayu
-  deepl
-  aria2gui
-  iina
-  qbittorrent
-  youdaodict
-  # jetbrains-toolbox
-  sublime-text
-  sublime-merge
-  visual-studio-code
-  nvalt
-  # qq
-  # qqmusic
-  # gimp
-  # telegram
-)
-brew install --cask "${cask[@]}"
+brew install wget
+brew install tree
+brew install ag
+brew install rg
+brew install git-lfs
+brew install coreutils
+brew install iterm2
+brew install google-chrome
+brew install utools
+brew install hammerspoon
+brew install pyenv
+brew install pipx
+brew install jq
+brew install aria2
+brew install ouch
 
-brew tap adoptopenjdk/openjdk
-openjdk=(
-  adoptopenjdk8
-  adoptopenjdk11
-)
-brew install --cask "${openjdk[@]}"
+brew install cmake
+brew install ninja
+brew install docker
+
+brew install visual-studio-code
+brew install topnotch
+brew install alt-tab
+brew install nvalt
+brew install iina
+brew install ImageOptim
+brew install hexyl
+brew install ImHex
+brew install HandBrake
+brew install TablePlus
+# brew install BetterAndBetter
+# brew install gimp
+# brew install qbittorrent
+# brew install telegram
+# brew install sublime-text
+# brew install sublime-merge
+
+brew install qq
+brew install wechat
+brew install qqmusic
+brew install neteasemusic
+# brew install youdaodict
+
+pipx install poetry
+pipx install ipython
+pipx install black
+pipx install ranger-fm
+pipx install conan
