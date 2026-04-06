53adf9febc2164f5ba793f78ff1c4090b8189d43
huoguangjin <352886006@qq.com>
2026-04-06 20:39:16 +0800

    feat(setup): update remove_keyboard_layout.py

--- setup/remove_keyboard_layout.py
+++ setup/remove_keyboard_layout.py
@@ -1,8 +1,32 @@
-import itertools
+#!/usr/bin/env python3
+import plistlib
 import shutil
-import subprocess
-import xml.etree.ElementTree as ET
 from pathlib import Path
+
+
+def remove_keyboard_layout(data, target_id):
+    if isinstance(data, dict):
+        if data.get("KeyboardLayout ID") == target_id:
+            return None
+
+        new_dict = {}
+        for k, v in data.items():
+            v = remove_keyboard_layout(v, target_id)
+            if v is not None:
+                new_dict[k] = v
+
+        return new_dict
+    elif isinstance(data, list):
+        new_list = []
+        for v in data:
+            v = remove_keyboard_layout(v, target_id)
+            if v is not None:
+                new_list.append(v)
+
+        return new_list
+    else:
+        return data
+
 
 plist = Path("~/Library/Preferences/com.apple.HIToolbox.plist").expanduser()
 backup = Path(str(plist) + ".bak")
@@ -10,24 +34,7 @@ if not backup.exists():
     print(f"backup: {backup}")
     shutil.copy(plist, backup)
 
-cmd = f"plutil -convert xml1 {plist} -o -"
-content = subprocess.getoutput(cmd)
-
-keyboard_layout_abc_id = "252"
-
-root = ET.fromstring(content)
-parent_nodes = root.findall(".//dict[key='KeyboardLayout ID']/..")
-for parent_node in parent_nodes:
-    dict_nodes = parent_node.findall("dict[key='KeyboardLayout ID']")
-    for dict_node in dict_nodes:
-        keyboard_layout = {
-            key_node.text: value_node.text
-            for key_node, value_node in itertools.batched(dict_node, 2)
-        }
-
-        if keyboard_layout["KeyboardLayout ID"] == keyboard_layout_abc_id:
-            print(f"remove: {keyboard_layout=}")
-            parent_node.remove(dict_node)
-
-modified_content = ET.tostring(root)
-plist.write_bytes(modified_content)
+keyboard_layout_abc_id = 252
+pl = plistlib.loads(plist.read_bytes())
+new_pl = remove_keyboard_layout(pl, keyboard_layout_abc_id)
+plist.write_bytes(plistlib.dumps(new_pl, fmt=plistlib.FMT_BINARY))
